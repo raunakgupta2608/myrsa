fc10b0f391d8ae180f96a2f14f159d15597ba4c2
Raunak Gupta <raunakgupta2608@gmail.com>
2020-08-18 21:47:31 +0530

    Quotes added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     MatCardModule,
+    MatIconModule,
     BrowserAnimationsModule
   ],
   providers: [],

f1a310ba2f995a4ff2284c660c3f32d469f1a1bf
Raunak Gupta <raunakgupta2608@gmail.com>
2020-08-18 20:52:42 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myrsa';
+
+  arr = [
+    {
+      quote: '"The Shiba Inu is the smallest of the six original and distinct spitz breeds"',
+      image: "https://images.unsplash.com/photo-1592009309602-1dde752490ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+      name: "- Eunice Splauding",
+      adj: "Creezy"
+    },
+    {
+      quote: '"The Shiba Inu is the smallest of the six original and distinct spitz breeds"',
+      image: "https://images.unsplash.com/photo-1592009309602-1dde752490ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+      name: "- Eunice Splauding",
+      adj: "Creezy"
+    },
+    {
+      quote: '"The Shiba Inu is the smallest of the six original and distinct spitz breeds"',
+      image: "https://images.unsplash.com/photo-1592009309602-1dde752490ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+      name: "- Eunice Splauding",
+      adj: "Creezy"
+    },
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatCardModule } from '@angular/material/card';
+
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
